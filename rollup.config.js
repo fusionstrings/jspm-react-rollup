6c49b963a4dc893a720d24d3bb6560def7c87037
Dilip Kr. Shukla <contact@fusionstrings.com>
2021-06-17 09:31:10 +0200

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -4,10 +4,13 @@ import babel from 'rollup-plugin-babel';
 export default {
   // Leading "./" still important here
   input: ['./app/main.js'],
-  output: {
+  output: [{
     dir: 'dist/rollup',
     format: 'system'
-  },
+  },{
+    dir: 'dist/rollup/esm',
+    format: 'esm'
+  }],
   plugins: [jspm({
     env: {
       node: true,
